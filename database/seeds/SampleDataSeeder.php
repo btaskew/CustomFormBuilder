f9f0da2dfbc145b0a0a59223d6fa87217e54a5a5
btaskew <btaskew@hotmail.co.uk>
2019-01-07 10:57:31 +0000

    Add seeders

---
+++ database/seeds/SampleDataSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Form;
+use App\Question;
+use Illuminate\Database\Seeder;
+
+class SampleDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $forms = factory(Form::class, 3)->create(['user_id' => 1]);
+
+        $forms->each(function (Form $form) {
+            factory(Question::class, 4)->create([
+                'form_id' => $form->id
+            ]);
+        });
+    }
+}
---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+            'id' => 1,
+            'name' => 'John Doe',
+            'email' => 'john@email.com',
+            'password' => bcrypt('password')
+        ]);
+    }
+}
