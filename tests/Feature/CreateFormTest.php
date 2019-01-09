b6f9f8fc6fe2824f0151948eb34ab3c08abfda7a
btaskew <btaskew@hotmail.co.uk>
2019-01-09 11:12:14 +0000

    Move tests

--- tests/Feature/CreateFormTest.php
+++ tests/Feature/CreateFormTest.php
@@ -17,14 +17,6 @@ class CreateFormTest extends TestCase
     }
 
     /** @test */
-    public function can_view_edit_form_page()
-    {
-        $form = $this->loginUserWithForm();
-
-        $this->get('/forms/' . $form->id . '/edit')->assertSee($form->title);
-    }
-
-    /** @test */
     public function a_guest_cant_create_new_forms()
     {
         $this->withExceptionHandling();
@@ -45,38 +37,5 @@ class CreateFormTest extends TestCase
             ->assertStatus(302);
 
         $this->assertDatabaseHas('forms', $attributes);
-    }
-
-    /** @test */
-    public function a_user_can_edit_their_form()
-    {
-        $form = $this->loginUserWithForm();
-
-        $this->patch('/forms/' . $form->id, ['title' => 'New title'])
-            ->assertStatus(200);
-
-        $this->assertEquals('New title', $form->fresh()->title);
-    }
-
-    /** @test */
-    public function a_user_can_clear_the_description_field()
-    {
-        $form = $this->loginUserWithForm();
-
-        $this->patch('/forms/' . $form->id, ['title' => 'New title', 'description' => ''])
-            ->assertStatus(200);
-
-        $this->assertEquals('', $form->fresh()->description);
-    }
-
-    /** @test */
-    public function a_user_cant_edit_another_users_form()
-    {
-        $this->withExceptionHandling();
-        $form = create(Form::class, ['user_id' => 999]);
-
-        $this->login()
-            ->patch('/forms/' . $form->id, ['title' => 'New title'])
-            ->assertStatus(403);
     }
 }
---
+++ tests/Feature/UpdateFormTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Form;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UpdateFormTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_view_edit_form_page()
+    {
+        $form = $this->loginUserWithForm();
+
+        $this->get('/forms/' . $form->id . '/edit')->assertSee($form->title);
+    }
+
+    /** @test */
+    public function a_user_can_edit_their_form()
+    {
+        $form = $this->loginUserWithForm();
+
+        $this->patch('/forms/' . $form->id, ['title' => 'New title'])
+            ->assertStatus(200);
+
+        $this->assertEquals('New title', $form->fresh()->title);
+    }
+
+    /** @test */
+    public function a_user_can_clear_the_description_field()
+    {
+        $form = $this->loginUserWithForm();
+
+        $this->patch('/forms/' . $form->id, ['title' => 'New title', 'description' => ''])
+            ->assertStatus(200);
+
+        $this->assertEquals('', $form->fresh()->description);
+    }
+
+    /** @test */
+    public function a_user_cant_edit_another_users_form()
+    {
+        $this->withExceptionHandling();
+        $form = create(Form::class, ['user_id' => 999]);
+
+        $this->login()
+            ->patch('/forms/' . $form->id, ['title' => 'New title'])
+            ->assertStatus(403);
+    }
+}
