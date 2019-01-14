5600f75c357d3e3ab27e2adbeed242b43dcdbb01
btaskew <btaskew@hotmail.co.uk>
2019-01-14 11:02:02 +0000

    Extract method

--- resources/assets/js/classes/fields/Field.js
+++ resources/assets/js/classes/fields/Field.js
@@ -11,11 +11,14 @@ export default class Field
         this.properties.label = this.question.title;
         this.properties.required = this.question.required;
         this.properties.help = this.question.help_text;
+        this.setVisibilityRequirement();
+    }
 
+    setVisibilityRequirement() {
         if (this.question.visibility_requirement) {
             const question = this.question.visibility_requirement.required_question_id;
             const value = this.question.visibility_requirement.required_value;
-            this.properties.visible = function(model) {
+            this.properties.visible = function (model) {
                 return model && model[question] == value;
             }
         }
