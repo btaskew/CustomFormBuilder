57e1ec8f9627ede68f8b384001ab671145379cdb
btaskew <btaskew@hotmail.co.uk>
2019-01-21 10:56:14 +0000

    Validate title for searching

--- app/Http/Controllers/QuestionSearchController.php
+++ app/Http/Controllers/QuestionSearchController.php
@@ -7,9 +7,15 @@ use Illuminate\Http\Request;
 
 class QuestionSearchController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return mixed
+     */
     public function show(Request $request)
     {
-        // TODO validate request title
+        $request->validate([
+            'title' => 'string|required'
+        ]);
 
         return Question::where('in_question_bank', true)
             ->where('title', 'like', '%' . $request->input('title') . '%')
