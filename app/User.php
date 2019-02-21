619e994f069aa51156de8544ef9e2b7e31b8545f
btaskew <btaskew@hotmail.co.uk>
2019-02-21 11:06:12 +0000

    Setup roles package

--- app/User.php
+++ app/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Collection;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
