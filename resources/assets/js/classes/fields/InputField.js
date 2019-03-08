4d6ae9bd00861d4bc3b3e8fa942db466a34a844c
btaskew <btaskew@hotmail.co.uk>
2019-03-08 15:14:00 +0000

    Add additional field validation

--- resources/assets/js/classes/fields/InputField.js
+++ resources/assets/js/classes/fields/InputField.js
@@ -1,4 +1,5 @@
 import Field from './Field';
+import VueFormGenerator from 'vue-form-generator';
 
 export default class InputField extends Field {
     constructor(question) {
@@ -9,6 +10,26 @@ export default class InputField extends Field {
         super.setDefaultProperties();
         this.properties.type = 'input';
         this.properties.inputType = this.question.type;
+        this.setValidation();
         return this.properties;
+    }
+
+    setValidation() {
+        switch (this.question.type) {
+            case 'email':
+                this.properties.validator = VueFormGenerator.validators.email;
+                break;
+            case 'number':
+                this.properties.validator = VueFormGenerator.validators.number;
+                break;
+            case 'url':
+                this.properties.validator = VueFormGenerator.validators.url.locale({
+                    invalidURL: "Invalid URL! Must start with http:// or https://"
+                });;
+                break;
+            case 'date':
+                this.properties.validator = VueFormGenerator.validators.date;
+                break;
+        }
     }
 }
